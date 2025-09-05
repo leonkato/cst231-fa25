bc65d0236bfd7dec8e5c74d969eec8c827695859
mark sausville <saus@ieee.org>
2025-09-04 18:41:54 -0700

    Minor edits.

--- wk01/d2/TheNullProgram.java
+++ wk01/d2/TheNullProgram.java
@@ -1,12 +1,6 @@
 package wk01.d2;
-import java.util.Scanner;
 
 public class TheNullProgram {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-
-        String myString = in.next();
-        System.out.println(myString);
-
     }
 }
