f6c11fc6267e80bea5c2052ca419f50130725192
mark sausville <saus@ieee.org>
2025-10-13 05:46:21 -0700

    add echo and echoln

---
+++ wk08/echo.java
@@ -0,0 +1,19 @@
+package wk08;
+import java.io.*;
+import java.util.Scanner;
+
+public class echo {
+    public static void main(String[] args) throws IOException {
+
+        Scanner userIn = new Scanner(System.in);
+        System.out.print("Enter filename: ");
+        String filename = userIn.next();;
+
+        FileReader fr = new FileReader(filename);
+        Scanner fileIn = new Scanner(fr);
+
+        while (fileIn.hasNext()) {
+            System.out.println(fileIn.next());
+        }
+    }
+}
---
+++ wk08/echoln.java
@@ -0,0 +1,21 @@
+package wk08;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class echoln {
+    public static void main(String[] args) throws IOException {
+
+        Scanner userIn = new Scanner(System.in);
+        System.out.print("Enter filename: ");
+        String filename = userIn.next();;
+
+        FileReader fr = new FileReader(filename);
+        Scanner fileIn = new Scanner(fr);
+
+        while (fileIn.hasNextLine()) {
+            System.out.println(fileIn.nextLine());
+        }
+    }
+}
